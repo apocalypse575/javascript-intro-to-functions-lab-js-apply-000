31fb7f9d83a74b06ea86d5283fb006e88123a94b
apocalypse575 <apocalypse575@gmail.com>
2017-09-10 01:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var string = 'hello'
+  var string = 'HELLO'
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!"}
-else if (string === string.toUpperCase()) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"}
   else if (string === "I love you, Grandma.") {
     return "I love you, too."}
