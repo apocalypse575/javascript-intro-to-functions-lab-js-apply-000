3c46bd380743646b266ab93e7378e0f51c4e2a91
apocalypse575 <apocalypse575@gmail.com>
2017-09-10 00:37:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var string = 'i love you'
+  if (string.toLowerCase === lowercase) {
+  return 'I can\'t hear you!'}
+}
